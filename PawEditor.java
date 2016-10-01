ba3b08b212997001d801635aa67a00ffbefa604d
treyzania <treyzania@gmail.com>
2016-10-01 13:07:40 -0400

    Added core classes and components.

---
+++ PawEditor.java
@@ -0,0 +1,16 @@
+
+public class PawEditor {
+	
+	private static PawEditor editor;
+		
+	public static void main(String[] args) {
+		
+		// TODO Do initialization stuff.
+		
+		editor = new PawEditor();
+		
+	}
+	
+	
+	
+}
